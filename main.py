98faa323216aea74b4031b75dda41ffbafc31b0c
Likho1969 <likhokapesi04@gmail.com>
2021-06-08 10:13:33 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+
+class MyMiniCalculator:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
+
+    def divide(self, a, b):
+        return a / b
+
+    def multiply(self, a, b):
+        return a * b
+
